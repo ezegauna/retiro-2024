990a6d461daf0439c6855541d6b0770c526e9ed5
Ezequiel Gauna <ezequiel.gauna@greencodesoftware.com>
2024-04-07 01:20:18 -0300

    wip

--- phrases/views.py
+++ phrases/views.py
@@ -1,9 +1,15 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 from .models import Frase
 import random
 
 
 def home(request):
-    frases = Frase.objects.all()
-    frase_aleatoria = random.choice(frases)
+    if 'frase_cookie' in request.COOKIES:
+        frase_aleatoria = request.COOKIES['frase_cookie']
+    else:
+        frases = Frase.objects.all()
+        frase_aleatoria = random.choice(frases)
+        response = HttpResponse()
+        response.set_cookie('frase_cookie', frase_aleatoria)
     return render(request, "home.html", {"frase_aleatoria": frase_aleatoria})
